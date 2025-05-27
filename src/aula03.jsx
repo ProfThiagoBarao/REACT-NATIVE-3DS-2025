969f08310240ed8b80275f0a131a344654b89b10
ProfThiagoBarao <126165664+ProfThiagoBarao@users.noreply.github.com>
2025-05-27 07:55:25 -0300

    Add files via upload

--- src/aula03.jsx
+++ src/aula03.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 class Aula03 extends Component {
+    constructor(props) {
+            super(props);
+            this.state = {
+                backgroundColor : 'green',
+            }
+        } 
+
+        changeColor = () => {
+            const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+            this.setState({ backgroundColor: randomColor });
+        }
+
     render() {
         return(
             <View style={ styles.container }>
-                <TouchableOpacity style={ styles.botao }>
-                    <Text>Botão</Text>
+                <TouchableOpacity 
+                style={ [ styles.botao, {backgroundColor: this.state.backgroundColor} ]}
+                onPress={ this.changeColor }
+                >
+                    <Text style={ styles.textoBotao }>Toque para mudar a cor do fundo</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -23,7 +39,14 @@ const styles = StyleSheet.create({
     },
 
     botao: {
-        backgroundColor: 'blue'
-    }
+        backgroundColor: 'blue',
+        padding: 15,
+        borderRadius: 5,
+    },
+
+    textoBotao: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
 
 })
